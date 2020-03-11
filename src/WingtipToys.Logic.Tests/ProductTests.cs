06d089bbfe3bde097fed9596d145a27bf4d9913d
Joseph Guadagno <jguadagno@hotmail.com>
2020-03-10 17:10:09 -0700

    Demo-UnitTests

---
+++ src/WingtipToys.Logic.Tests/ProductTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace WingtipToys.Logic.Tests
+{
+    public class ProductTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ src/WingtipToys.Logic.Tests/UserTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace WingtipToys.Logic.Tests
+{
+    public class UserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
