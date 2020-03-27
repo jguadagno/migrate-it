e7ced6ee671986a9e6dfe94f8e4f7f5316fd6875
Joseph Guadagno <jguadagno@hotmail.com>
2020-03-27 16:58:30 -0700

    Demo-WebUiCategoryMenu

---
+++ src/WingtipToys.WebUi/Pages/Shared/Components/CategoryList/CategoryList.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WingtipToys.WebUi.Pages.Shared.Components.CategoryList
+{
+    public class CategoryList : ViewComponent
+    {
+        private readonly Domain.IProductManager _productManager;
+
+        public CategoryList(Domain.IProductManager productManager)
+        {
+            _productManager = productManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _productManager.GetCategories();
+            return View(categories);
+        }
+    }
+}
